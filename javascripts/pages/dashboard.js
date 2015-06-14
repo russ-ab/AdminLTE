225035f17ddacf24e556bb20ef959a87b96a900c
Weihong Guan <weihong.guan@gmail.com>
2015-06-14 08:16:31 +0800

    jshint correction for js

--- javascripts/pages/dashboard.js
+++ javascripts/pages/dashboard.js
@@ -1,33 +1,36 @@
+/* globals Morris: false */
+/* globals moment: false */
+
 /*
  * Author: Abdullah A Almsaeed
  * Date: 4 Jan 2014
  * Description:
  *      This is a demo file used only for the main dashboard (index.html)
  **/
-"use strict";
+'use strict';
 
 $(function () {
 
   //Make the dashboard widgets sortable Using jquery UI
-  $(".connectedSortable").sortable({
-    placeholder: "sort-highlight",
-    connectWith: ".connectedSortable",
-    handle: ".box-header, .nav-tabs",
+  $('.connectedSortable').sortable({
+    placeholder: 'sort-highlight',
+    connectWith: '.connectedSortable',
+    handle: '.box-header, .nav-tabs',
     forcePlaceholderSize: true,
     zIndex: 999999
   });
-  $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+  $('.connectedSortable .box-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move');
   
   //jQuery UI sortable for the todo list
-  $(".todo-list").sortable({
-    placeholder: "sort-highlight",
-    handle: ".handle",
+  $('.todo-list').sortable({
+    placeholder: 'sort-highlight',
+    handle: '.handle',
     forcePlaceholderSize: true,
     zIndex: 999999
   });
 
   //bootstrap WYSIHTML5 - text editor
-  $(".textarea").wysihtml5();
+  $('.textarea').wysihtml5();
 
   $('.daterange').daterangepicker(
           {
@@ -43,49 +46,50 @@ $(function () {
             endDate: moment()
           },
   function (start, end) {
-    alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    alert('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
   });
 
   /* jQueryKnob */
-  $(".knob").knob();
+  $('.knob').knob();
 
   //jvectormap data
   var visitorsData = {
-    "US": 398, //USA
-    "SA": 400, //Saudi Arabia
-    "CA": 1000, //Canada
-    "DE": 500, //Germany
-    "FR": 760, //France
-    "CN": 300, //China
-    "AU": 700, //Australia
-    "BR": 600, //Brazil
-    "IN": 800, //India
-    "GB": 320, //Great Britain
-    "RU": 3000 //Russia
+    'US': 398, //USA
+    'SA': 400, //Saudi Arabia
+    'CA': 1000, //Canada
+    'DE': 500, //Germany
+    'FR': 760, //France
+    'CN': 300, //China
+    'AU': 700, //Australia
+    'BR': 600, //Brazil
+    'IN': 800, //India
+    'GB': 320, //Great Britain
+    'RU': 3000 //Russia
   };
   //World map by jvectormap
   $('#world-map').vectorMap({
     map: 'world_mill_en',
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     regionStyle: {
       initial: {
         fill: '#e4e4e4',
-        "fill-opacity": 1,
+        'fill-opacity': 1,
         stroke: 'none',
-        "stroke-width": 0,
-        "stroke-opacity": 1
+        'stroke-width': 0,
+        'stroke-opacity': 1
       }
     },
     series: {
       regions: [{
           values: visitorsData,
-          scale: ["#92c1dc", "#ebf4f9"],
+          scale: ['#92c1dc', '#ebf4f9'],
           normalizeFunction: 'polynomial'
         }]
     },
     onRegionLabelShow: function (e, el, code) {
-      if (typeof visitorsData[code] != "undefined")
+      if (typeof visitorsData[code] !== 'undefined') {
         el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
+      }
     }
   });
 
@@ -94,7 +98,7 @@ $(function () {
   $('#sparkline-1').sparkline(myvalues, {
     type: 'line',
     lineColor: '#92c1dc',
-    fillColor: "#ebf4f9",
+    fillColor: '#ebf4f9',
     height: '50',
     width: '80'
   });
@@ -102,7 +106,7 @@ $(function () {
   $('#sparkline-2').sparkline(myvalues, {
     type: 'line',
     lineColor: '#92c1dc',
-    fillColor: "#ebf4f9",
+    fillColor: '#ebf4f9',
     height: '50',
     width: '80'
   });
@@ -110,13 +114,13 @@ $(function () {
   $('#sparkline-3').sparkline(myvalues, {
     type: 'line',
     lineColor: '#92c1dc',
-    fillColor: "#ebf4f9",
+    fillColor: '#ebf4f9',
     height: '50',
     width: '80'
   });
 
   //The Calender
-  $("#calendar").datepicker();
+  $('#calendar').datepicker();
 
   //SLIMSCROLL FOR CHAT WIDGET
   $('#chat-box').slimScroll({
@@ -146,7 +150,8 @@ $(function () {
     lineColors: ['#a0d0e0', '#3c8dbc'],
     hideHover: 'auto'
   });
-  var line = new Morris.Line({
+  
+  new Morris.Line({
     element: 'line-chart',
     resize: true,
     data: [
@@ -167,12 +172,12 @@ $(function () {
     lineColors: ['#efefef'],
     lineWidth: 2,
     hideHover: 'auto',
-    gridTextColor: "#fff",
+    gridTextColor: '#fff',
     gridStrokeWidth: 0.4,
     pointSize: 4,
-    pointStrokeColors: ["#efefef"],
-    gridLineColor: "#efefef",
-    gridTextFamily: "Open Sans",
+    pointStrokeColors: ['#efefef'],
+    gridLineColor: '#efefef',
+    gridTextFamily: 'Open Sans',
     gridTextSize: 10
   });
 
@@ -180,11 +185,11 @@ $(function () {
   var donut = new Morris.Donut({
     element: 'sales-chart',
     resize: true,
-    colors: ["#3c8dbc", "#f56954", "#00a65a"],
+    colors: ['#3c8dbc', '#f56954', '#00a65a'],
     data: [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
+      {label: 'Download Sales', value: 12},
+      {label: 'In-Store Sales', value: 30},
+      {label: 'Mail-Order Sales', value: 20}
     ],
     hideHover: 'auto'
   });
@@ -195,39 +200,13 @@ $(function () {
     donut.redraw();
   });
 
-
-  /* BOX REFRESH PLUGIN EXAMPLE (usage with morris charts) */
-  $("#loading-example").boxRefresh({
-    source: "ajax/dashboard-boxrefresh-demo.php",
-    onLoadDone: function (box) {
-      bar = new Morris.Bar({
-        element: 'bar-chart',
-        resize: true,
-        data: [
-          {y: '2006', a: 100, b: 90},
-          {y: '2007', a: 75, b: 65},
-          {y: '2008', a: 50, b: 40},
-          {y: '2009', a: 75, b: 65},
-          {y: '2010', a: 50, b: 40},
-          {y: '2011', a: 75, b: 65},
-          {y: '2012', a: 100, b: 90}
-        ],
-        barColors: ['#00a65a', '#f56954'],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['CPU', 'DISK'],
-        hideHover: 'auto'
-      });
-    }
-  });
-
   /* The todo list plugin */
-  $(".todo-list").todolist({
+  $('.todo-list').todolist({
     onCheck: function (ele) {
-      console.log("The element has been checked")
+      console.log('The element has been checked');
     },
     onUncheck: function (ele) {
-      console.log("The element has been unchecked")
+      console.log('The element has been unchecked');
     }
   });
 
--- javascripts/pages/dashboard2.js
+++ javascripts/pages/dashboard2.js
@@ -1,3 +1,5 @@
+/* globals Chart: false */
+
 'use strict';
 $(function () {
 
@@ -11,31 +13,31 @@ $(function () {
   //-----------------------
 
   // Get context with jQuery - using jQuery's .get() method.
-  var salesChartCanvas = $("#salesChart").get(0).getContext("2d");
+  var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
   // This will get the first returned node in the jQuery collection.
   var salesChart = new Chart(salesChartCanvas);
 
   var salesChartData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
       {
-        label: "Electronics",
-        fillColor: "rgb(210, 214, 222)",
-        strokeColor: "rgb(210, 214, 222)",
-        pointColor: "rgb(210, 214, 222)",
-        pointStrokeColor: "#c1c7d1",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgb(220,220,220)",
+        label: 'Electronics',
+        fillColor: 'rgb(210, 214, 222)',
+        strokeColor: 'rgb(210, 214, 222)',
+        pointColor: 'rgb(210, 214, 222)',
+        pointStrokeColor: '#c1c7d1',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgb(220,220,220)',
         data: [65, 59, 80, 81, 56, 55, 40]
       },
       {
-        label: "Digital Goods",
-        fillColor: "rgba(60,141,188,0.9)",
-        strokeColor: "rgba(60,141,188,0.8)",
-        pointColor: "#3b8bba",
-        pointStrokeColor: "rgba(60,141,188,1)",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(60,141,188,1)",
+        label: 'Digital Goods',
+        fillColor: 'rgba(60,141,188,0.9)',
+        strokeColor: 'rgba(60,141,188,0.8)',
+        pointColor: '#3b8bba',
+        pointStrokeColor: 'rgba(60,141,188,1)',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(60,141,188,1)',
         data: [28, 48, 40, 19, 86, 27, 90]
       }
     ]
@@ -47,7 +49,7 @@ $(function () {
     //Boolean - Whether grid lines are shown across the chart
     scaleShowGridLines: false,
     //String - Colour of the grid lines
-    scaleGridLineColor: "rgba(0,0,0,.05)",
+    scaleGridLineColor: 'rgba(0,0,0,.05)',
     //Number - Width of the grid lines
     scaleGridLineWidth: 1,
     //Boolean - Whether to show horizontal lines (except X axis)
@@ -73,7 +75,7 @@ $(function () {
     //Boolean - Whether to fill the dataset with a color
     datasetFill: true,
     //String - A legend template
-    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
+    legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%=datasets[i].label%></li><%}%></ul>',
     //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
     maintainAspectRatio: false,
     //Boolean - whether to make the chart responsive to window resizing
@@ -91,51 +93,51 @@ $(function () {
   //- PIE CHART -
   //-------------
   // Get context with jQuery - using jQuery's .get() method.
-  var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+  var pieChartCanvas = $('#pieChart').get(0).getContext('2d');
   var pieChart = new Chart(pieChartCanvas);
   var PieData = [
     {
       value: 700,
-      color: "#f56954",
-      highlight: "#f56954",
-      label: "Chrome"
+      color: '#f56954',
+      highlight: '#f56954',
+      label: 'Chrome'
     },
     {
       value: 500,
-      color: "#00a65a",
-      highlight: "#00a65a",
-      label: "IE"
+      color: '#00a65a',
+      highlight: '#00a65a',
+      label: 'IE'
     },
     {
       value: 400,
-      color: "#f39c12",
-      highlight: "#f39c12",
-      label: "FireFox"
+      color: '#f39c12',
+      highlight: '#f39c12',
+      label: 'FireFox'
     },
     {
       value: 600,
-      color: "#00c0ef",
-      highlight: "#00c0ef",
-      label: "Safari"
+      color: '#00c0ef',
+      highlight: '#00c0ef',
+      label: 'Safari'
     },
     {
       value: 300,
-      color: "#3c8dbc",
-      highlight: "#3c8dbc",
-      label: "Opera"
+      color: '#3c8dbc',
+      highlight: '#3c8dbc',
+      label: 'Opera'
     },
     {
       value: 100,
-      color: "#d2d6de",
-      highlight: "#d2d6de",
-      label: "Navigator"
+      color: '#d2d6de',
+      highlight: '#d2d6de',
+      label: 'Navigator'
     }
   ];
   var pieOptions = {
     //Boolean - Whether we should show a stroke on each segment
     segmentShowStroke: true,
     //String - The colour of each segment stroke
-    segmentStrokeColor: "#fff",
+    segmentStrokeColor: '#fff',
     //Number - The width of each segment stroke
     segmentStrokeWidth: 1,
     //Number - The percentage of the chart that we cut out of the middle
@@ -143,7 +145,7 @@ $(function () {
     //Number - Amount of animation steps
     animationSteps: 100,
     //String - Animation easing effect
-    animationEasing: "easeOutBounce",
+    animationEasing: 'easeOutBounce',
     //Boolean - Whether we animate the rotation of the Doughnut
     animateRotate: true,
     //Boolean - Whether we animate scaling the Doughnut from the centre
@@ -153,9 +155,9 @@ $(function () {
     // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
     maintainAspectRatio: false,
     //String - A legend template
-    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+    legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
     //String - A tooltip template
-    tooltipTemplate: "<%=value %> <%=label%> users"
+    tooltipTemplate: '<%=value %> <%=label%> users'
   };
   //Create pie or douhnut chart
   // You can switch between pie and douhnut using the method below.  
@@ -177,13 +179,13 @@ $(function () {
     regionStyle: {
       initial: {
         fill: 'rgba(210, 214, 222, 1)',
-        "fill-opacity": 1,
+        'fill-opacity': 1,
         stroke: 'none',
-        "stroke-width": 0,
-        "stroke-opacity": 1
+        'stroke-width': 0,
+        'stroke-opacity': 1
       },
       hover: {
-        "fill-opacity": 0.7,
+        'fill-opacity': 0.7,
         cursor: 'pointer'
       },
       selected: {
